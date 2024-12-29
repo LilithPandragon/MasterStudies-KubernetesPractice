564cc7557020ae01d53844b0ed422e8f1a3568bd
pprugger <patrick.prugger@uname.at>
2024-12-29 18:07:49 +0100

    Update server.js to use environment variable for consumer port
    - Changed the port configuration in server.js from `process.env.PORT` to `process.env.CONSUMER_PORT`

--- consumer/server.js
+++ consumer/server.js
@@ -185,7 +185,7 @@ app.get('/', (req, res) => {
     `);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.CONSUMER_PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
     console.log('RABBITMQ_URL:', RABBITMQ_URL);
